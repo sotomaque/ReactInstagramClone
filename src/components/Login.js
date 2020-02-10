c8ead64d81b387769eba07fc50917aa444e8d880
sotomaque <sotomaque@gmail.com>
2020-02-10 11:20:22 -0800

    started to build out html structure of IG clone

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div>
+            <h2>Login</h2>
+            <form>
+                <input
+                    placeholder="enter a username"
+                    />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login; 
